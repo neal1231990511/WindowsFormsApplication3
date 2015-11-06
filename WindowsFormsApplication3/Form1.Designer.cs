0dee1c34f86639de1eb2cbd6026da892c6f1f912
Neal <neal1231990511@gmail.com>
2015-11-06 13:43:31 +0800

    5button

--- WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -84,7 +84,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(303, 261);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
